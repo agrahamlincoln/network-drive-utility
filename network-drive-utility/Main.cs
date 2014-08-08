c61bc2cce77917270231cb20fc8fb80cccaba678
agrahamlincoln <gtrounds@gmail.com>
2014-08-08 09:51:15 -0400

    First commit w/ C# Solution

---
+++ network-drive-utility/Main.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace network_drive_utility
+{
+    /// <summary>Primary class of the application, handles higher level program logic.
+    /// </summary>
+    class Main
+    {
+        /// <summary>Main Entrypoint for the Program
+        /// </summary>
+        /// <param name="args">Program arguments</param>
+        public void Main(string[] args)
+        {
+            // 1: Get list of fileshares from WMI
+            // 2: Get currently logged in user
+            // 3: Get User's fileshares from XML
+            // 4: Combine lists and update object from file
+            // 5: Write object to XML file
+        }
+    }
+}
